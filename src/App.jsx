9230766b2460e4d4aab4ff2ffd84492cd5c695f7
hajongon <hajongon@gmail.com>
2024-04-09 15:55:32 +0900

    basename 추가

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
     }, [navbarPosition])
 
     return (
-        <Router basename={'/'}>
+        <Router basename={'/vite-falcon-boilerplate'}>
             <FalconRoutes />
             <ToastContainer
                 closeButton={CloseButton}
